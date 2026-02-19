a859cd886e55a3fb8171f48d57eec4e6dcf759e7
Pawan Malik <Pawan.malik@cloudsufi.com>
2026-02-20 00:01:24 +0530

    transforming the basic agent from module 1 to a specialized agent

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,8 +1,27 @@
+# from google.adk.agents.llm_agent import Agent
+
+# root_agent = Agent(
+#     model='gemini-2.5-flash',
+#     name='root_agent',
+#     description='A helpful assistant for user questions.',
+#     instruction='Answer user questions to the best of your knowledge',
+# )
+
+
+
 from google.adk.agents.llm_agent import Agent
 
+
 root_agent = Agent(
+
     model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+
+    name='math_tutor_agent',
+
+    description='Helps students learn algebra by guiding them through problemsolving steps.',
+
+    instruction='You are a patient math tutor. Help students with algebra problems.'
+
 )
+3
+## trasnforming the basic agent from module 1 to specific agent for tutoring math. We have changed the name, description and instruction to make it more specific to the task of tutoring math.
